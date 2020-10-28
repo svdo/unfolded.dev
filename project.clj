101d8478cd9abdc40c27ea979b324a36c8221b38
Stefan van den Oord <soord@mac.com>
2020-10-28 11:41:14 +0100

    Updates project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cryogen "0.1.0"
-            :description "Simple static site generator"
-            :url "https://github.com/lacarmen/cryogen"
+(defproject unfolded-dev "0.1.0"
+            :description "My personal blog Unfolded.dev"
+            :url "https://unfolded.dev"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.10.1"]
