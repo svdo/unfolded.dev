e8caab325c01ac8d8bee02d5d25972f441129397
Stefan van den Oord <soord@mac.com>
2025-08-27 08:49:55 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
             :url "https://unfolded.dev"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.10.3"]
-                           [ring/ring-devel "1.9.3"]
-                           [compojure "1.6.2"]
+            :dependencies [[org.clojure/clojure "1.12.2"]
+                           [ring/ring-devel "1.14.2"]
+                           [compojure "1.7.1"]
                            [ring-server "0.5.0"]
-                           [cryogen-flexmark "0.1.4"]
-                           [cryogen-core "0.4.0"]]
-            :plugins [[lein-ring "0.12.5"]]
+                           [cryogen-flexmark "0.1.5"]
+                           [cryogen-core "0.4.6"]]
+            :plugins [[lein-ring "0.12.6"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
                    :handler cryogen.server/handler}
