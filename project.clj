a1977fbb6d9bde6ef0a6bfcf82f6391f8e055efa
Stefan van den Oord <stefan@viduet.eu>
2025-10-29 10:27:34 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
             :url "https://unfolded.dev"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.12.2"]
-                           [ring/ring-devel "1.14.2"]
-                           [compojure "1.7.1"]
+            :dependencies [[org.clojure/clojure "1.12.3"]
+                           [ring/ring-devel "1.15.3"]
+                           [compojure "1.7.2"]
                            [ring-server "0.5.0"]
                            [cryogen-flexmark "0.1.5"]
                            [cryogen-core "0.4.6"]]
